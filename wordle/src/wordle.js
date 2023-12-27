ae6534a6c4e0270fe05bb419abda142ab15d9725
william <william@laptop>
2023-12-27 23:13:20 +1300

    task 2, finished console button names when using mouse click on the buttons

--- wordle/src/wordle.js
+++ wordle/src/wordle.js
@@ -1,6 +1,34 @@
 
+document.addEventListener("DOMContentLoaded", function() {
+
+    // whe  DOM loaded , and then run the js code
 <!--task 1 , test the connection of css and js -->
 
-function oneDemo(){
-    alert('this is a successfull demo')
-}
+// function oneDemo(){
+//     alert('this is a successful demo')
+// }
+
+<!--task 2 started -->
+document.querySelector('#keyboard').addEventListener('click', function(event) {
+    if (event.target.classList.contains('clickBtn')) {
+        console.log(event.target.textContent);
+    }
+});
+<!--task 2 ended -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
