b331c83ac6bf943c88cc8209765b5c5ee00ac7c9
stevehu <stevehuinnz@gmail.com>
2023-12-28 23:06:27 +1300

    computer keyboard event done

--- wordle/src/wordle.js
+++ wordle/src/wordle.js
@@ -14,7 +14,27 @@ document.querySelector('#keyboard').addEventListener('click', function(event) {
         console.log(event.target.textContent);
     }
 });
-<!--task 2 ended -->
+
+// set the computer keyboard event
+    document.addEventListener('keydown', function(event) {
+        // Check if the key is one of A-Z, Enter, Backspace, or Escape
+        if ((event.key >= 'a' && event.key <= 'z') ||
+            event.key === 'Enter' ||
+            event.key === 'Backspace' ||
+            event.key === 'Escape') {
+
+            // Special handling for the Escape key
+            if (event.key === 'Escape') {
+                console.log('New Game');
+                // Here, add the logic for starting a new game
+            } else {
+                // Print the name of the key
+                console.log(event.key);
+            }
+        }
+    });
+
+    <!--task 2 ended -->
 
 
 
