54778b8625c6e9cbb6368a7d0f07c1314b0b27f4
stevehu <stevehuinnz@gmail.com>
2023-12-31 13:57:22 +1300

    task 4 set up the input area can enter words by using screen keyboard and the computer keyboard

--- wordle/src/wordle.js
+++ wordle/src/wordle.js
@@ -9,22 +9,68 @@ document.addEventListener("DOMContentLoaded", function() {
 // }
 
 
+    // const keys and then use for loop to console key names
+    const keys = document.querySelectorAll(".row button");
+
+
+    let guessedWords =[[]];
+    let availableSpace = 1;
+
     <!--task 3 started -->
     let word = "TARGET"
 
     // <!--task 3 finished  -->
+ // a function handleSubmitWord started
+    function handleSubmitWord(){
+        const currentWordArr = getCurrentWordArr();
+        if(currentWordArr.length !== 6){
+            window.alert("word must be 6 letters");
+        }
+        // combine all the elements of the array into one continuous string without any separator,
+        const currentWord = currentWordArr.join("");
+        if(currentWord === word){
+            window.alert("congratulations!");
+        }X
 
+        if (guessedWords.length === 6){
+            window.alert(`sorry, you have no more 6 guesses! the word is ${word}.`)
+        }
+        guessedWords.push([]);
 
+    }
 
-
+    // a function handleSubmitWord finished
 
 // <!--task 2 started -->
     // 1, set screen keyboard  keypress event
-document.querySelector('#keyboard').addEventListener('click', function(event) {
-    if (event.target.classList.contains('clickBtn')) {
-        console.log(event.target.textContent);
+// document.querySelector('#keyboard').addEventListener('click', function(event) {
+//     if (event.target.classList.contains('clickBtn')) {
+//         console.log(event.target.textContent);
+//     }
+// });
+
+    // const keys and then use for loop to console key names
+    for (let i = 0; i< keys.length; i++) {
+        keys[i].onclick = ({target}) => {
+            // const key = target.getAttribute("id");
+            const letter = target.getAttribute("id");
+
+            // console.log(key);
+            // this is to decide if the word is 5 digital, if it's not the function handlesubmitword will be applyed and the
+            // window will alert , please type  5 letter words
+            if(letter ==='enter'){
+                handleSubmitWord()
+                return;
+            }
+            updateGuessedWords(letter)
+        };
+
     }
-});
+
+
+
+
+
 
 // 2set the computer keyboard event
     document.addEventListener('keydown', function(event) {
@@ -40,7 +86,11 @@ document.querySelector('#keyboard').addEventListener('click', function(event) {
                 // Here, add the logic for starting a new game
             } else {
                 // Print the name of the key
-                console.log(event.key.toUpperCase());
+                // console.log(event.key.toUpperCase());
+
+                const letter = event.key.toUpperCase();
+
+                updateGuessedWords(letter)
             }
         }
     });
@@ -71,11 +121,13 @@ document.querySelector('#keyboard').addEventListener('click', function(event) {
 
     <!--task 4 'current guess' started -->
 // generate 30 squares
+
+    createSquares()
     function createSquares(){
         const gameBoard = document.getElementById("board")
 
 
-        for (let index =0;index<30; index++){
+        for (let index =0;index<36; index++){
             let square =document.createElement("div");
             square.classList.add("square");
             square.setAttribute("id",index + 1);
@@ -83,6 +135,27 @@ document.querySelector('#keyboard').addEventListener('click', function(event) {
 
         }
     }
+
+    //  update the guessed words
+    function getCurrentWordArr(){
+        const numberOfGuessedWords = guessedWords.length
+        return guessedWords[numberOfGuessedWords-1]
+    }
+
+    function updateGuessedWords (letter){
+        const currentWordArr= getCurrentWordArr()
+        if(currentWordArr&&currentWordArr.length < 6){
+            currentWordArr.push(letter)
+            const availableSpaceEl=document.getElementById(String(availableSpace))
+            availableSpace = availableSpace + 1;
+            availableSpaceEl.textContent = letter;
+        }
+
+
+    }
+
+
+
     <!--task 4 'current guess' ended -->
 
 
